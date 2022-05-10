54558098437d42fcd2b266c0579808be681e5775
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-05-10 17:20:33 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,19 @@
-// Create API Config here ...
-import axios from 'axios';
+export const API = () => {
+    const baseUrl =
+        process.env.REACT_APP_SERVER_URL ||
+        'https://dumbmerch-id.herokuapp.com/api/v1' ||
+        'https://localhost:5000/api/v1';
 
-export const API = axios.create({
-    baseURL: 'http://localhost:5000/api/v1/',
-});
+    const executeAPI = async (endpoint, config) => {
+        const response = await fetch(baseUrl + endpoint, config);
+        const data = await response.json();
+        return data;
+    };
 
-export const setAuthToken = (token) => {
-    if (token) {
-        API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    } else {
-        delete API.defaults.headers.commin['Authorization'];
-    }
+    return {
+        get: executeAPI,
+        post: executeAPI,
+        patch: executeAPI,
+        delete: executeAPI,
+    };
 };
