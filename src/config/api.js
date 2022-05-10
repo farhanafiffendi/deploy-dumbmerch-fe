8e8cdb4adec8b39b2c769c427715db4a6a5a3f85
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-05-10 21:06:35 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL || 'https://dumbmerch-co.herokuapp.com//api/v1'
+    baseURL: process.env.REACT_APP_SERVER_URL || 'https://dumbmerch-king.herokuapp.com/api/v1'
         || 'http://localhost:5000/api/v1',
 });
 
